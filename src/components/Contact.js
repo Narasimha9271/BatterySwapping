b8dd817db57ba271e8400dabc14279056f925bc2
Syamsai-Reddy <syamsaireddyyeturu@gmail.com>
2023-02-27 21:22:41 +0530

    commitedbysyamsai

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      <div className="text-6xl text-center mt-20 mb-20 ">This is CONTACT Page </div>
+      <div className="text-6xl text-center mt-20 mb-20 ">This is CONTACT Page 
+      built   by syamsai </div>
     </div>
   )
 }
