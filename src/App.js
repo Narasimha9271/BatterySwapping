130f29c23c41ca6abc150657b095e72abf59b494
Narasimha P L <107346073+Narasimha9271@users.noreply.github.com>
2023-03-31 15:44:09 +0530

    added starter-maps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
@@ -8,50 +8,53 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 import SwapStations from "./components/SwapStations";
 import Team from "./components/Team";
+import { ChakraProvider } from "@chakra-ui/provider";
+import { theme } from "@chakra-ui/theme";
 
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
+};
 
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/about",
+                element: <About />,
+            },
+            {
+                path: "/",
+                element: <Body />,
+            },
 
-const App=()=>{
-        return(
-            <>
-                <Header />
-                <Outlet />
-                <Footer />
-            </>
-        )
-    }
+            {
+                path: "/contact",
+                element: <Contact />,
+            },
+            {
+                path: "/team",
+                element: <Team />,
+            },
+            {
+                path: "/stations",
+                element: (
+                    <ChakraProvider theme={theme}>
+                        <SwapStations />
+                    </ChakraProvider>
+                ),
+            },
+        ],
+    },
+]);
 
-    const appRouter = createBrowserRouter([
-        {
-            path:"/",
-            element:<App/>,
-            children:[
-                {
-                    path:"/about",
-                    element:<About />
-                },
-                {
-                    path:"/",
-                    element:<Body />
-                },
-                
-                {
-                    path:"/contact",
-                    element:<Contact />
-                },
-                {
-                    path:"/team",
-                    element:<Team />
-                },
-                {
-                    path:"/stations",
-                    element:<SwapStations />
-                }
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-            ]
-        }
-    ])
-
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-
-  root.render(<RouterProvider router={appRouter} />);
+root.render(<RouterProvider router={appRouter} />);
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,96 +1,98 @@
 import React from "react";
+import swapLogo from "../assets/img/swapping.gif";
 
 const Body = () => {
-  return (
-    <div>
-      <h1 className="text-6xl mt-5 text-center text-green-300">
-        Swapping made Fast
-      </h1>
-      <h1 className="text-3xl mt-1 mb-60 text-center text-green-300">
-        Operate your electric vehicle fleet with zero downtime for charging.
-      </h1>
-      <img
-        className="w-full align-middle"
-        src="https://www.voltup.in/assets/images/gif/swapping.gif"
-        alt="logo"
-      />
+    return (
+        <div>
+            <h1 className="text-6xl mt-5 text-center text-green-300">
+                Swapping made Fast
+            </h1>
+            <h1 className="text-3xl mt-7 mb-40 text-center text-green-300">
+                Operate your electric vehicle fleet with zero downtime for
+                charging.
+            </h1>
+            <img
+                className="w-full align-middle "
+                src={swapLogo}
+                alt="swap-logo"
+            />
 
-      <div className=" gap-x-[2.116vw] flex flex-wrap justify-center gap-y-[2.116vw] m-20">
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/3.32ebcf2a1dac9ae5fbf23e97713895b6.svg"
-              alt="battery-logo"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            Smart and Interoperable Li-ion batteries
-          </div>
+            <div className=" gap-x-[2.116vw] flex flex-wrap justify-center gap-y-[2.116vw] m-20">
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/3.32ebcf2a1dac9ae5fbf23e97713895b6.svg"
+                            alt="battery-logo"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        Smart and Interoperable Li-ion batteries
+                    </div>
+                </div>
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/2.fa5d0c066eeaa94cf43d43395756f82e.svg"
+                            alt="logo"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        Wide network of battery swapping stations
+                    </div>
+                </div>
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/1.380a394dc4e9329274d4093669ec085e.svg"
+                            alt="log"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        2 minute quick battery swapping
+                    </div>
+                </div>
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/4.55aabd1d51107dccabc59086e362f00b.svg"
+                            alt="logo-pic"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        Compatible with both electric three and two wheelers
+                    </div>
+                </div>
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/5.e5ff2e608f1e512e0f55b7bfa8ecedcb.svg"
+                            alt="pic"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        Driver and Partner Mobile Apps
+                    </div>
+                </div>
+                <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
+                    <div>
+                        <img
+                            className=" h-[6vw] w-[6vw] float-left align-middle border-none"
+                            src="https://www.batterysmart.in/static/media/6.849254eb327b7bc63f7c63fcc7cef712.svg"
+                            alt="log-pic"
+                        />
+                    </div>
+                    <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
+                        Supports new & retrofit vehicle models
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/2.fa5d0c066eeaa94cf43d43395756f82e.svg"
-              alt="logo"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            Wide network of battery swapping stations
-          </div>
-        </div>
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/1.380a394dc4e9329274d4093669ec085e.svg"
-              alt="log"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            2 minute quick battery swapping
-          </div>
-        </div>
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/4.55aabd1d51107dccabc59086e362f00b.svg"
-              alt="logo-pic"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            Compatible with both electric three and two wheelers
-          </div>
-        </div>
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/5.e5ff2e608f1e512e0f55b7bfa8ecedcb.svg"
-              alt="pic"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            Driver and Partner Mobile Apps
-          </div>
-        </div>
-        <div className=" items-center shadow-[0_8px_24px_rgba(0,0,0,0.12)] flex justify-center text-center p-4  rounded-2xl  bg-white ">
-          <div>
-            <img
-              className=" h-[6vw] w-[6vw] float-left align-middle border-none"
-              src="https://www.batterysmart.in/static/media/6.849254eb327b7bc63f7c63fcc7cef712.svg"
-              alt="log-pic"
-            />
-          </div>
-          <div className="text-[1.2vw] text-left w-[18vw] ml-[5px] px-2.5 py-0">
-            Supports new & retrofit vehicle models
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Body;
