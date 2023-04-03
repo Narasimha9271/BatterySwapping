53bf8a18f43b0b4d60d1a9d80af58adde3fc46c9
Narasimha P L <107346073+Narasimha9271@users.noreply.github.com>
2023-04-03 11:27:18 +0530

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Team = () => {
-  return (
-    <div>
-      <div className="text-6xl text-center mt-20 mb-20 ">This is Our Team Page </div>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="text-6xl text-center mt-20 mb-20 ">
+                This is Our Team Page{" "}
+            </div>
+        </div>
+    );
+};
 
-export default Team
+export default Team;
